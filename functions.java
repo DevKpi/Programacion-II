6e9abdd2d0ea1419169217908115f5d48aaec61e
Kpi <186628550+DevKpi@users.noreply.github.com>
2026-04-29 20:33:32 -0300

    Add files via upload

---
+++ functions.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kpi;
+import java.util.*;
+
+public class functions {
+    public static void main23(String[] args){
+        
+        // Usando for
+        /*System.out.println("Ingrese un número");
+        Scanner entrada = new Scanner(System.in);
+        
+        int num = entrada.nextInt();
+        
+        for(int i = 1; i <= 10; i++){
+            System.out.println(num * i);
+        }*/
+        
+        
+        // Usando while
+        /*System.out.println("Ingrese un PIN de 4 números:");
+        Scanner entrada = new Scanner(System.in);
+        
+        int pinIngresado = entrada.nextInt();
+        
+        int pinCorrecto = 1346;
+        
+        while( pinIngresado != pinCorrecto){
+            System.out.println("El pin ingresado es incorrecto, intenta nuevamente: ");
+            Scanner entrada2 = new Scanner(System.in);
+            pinIngresado = entrada2.nextInt();
+        }
+        
+        System.out.println("Ingresaste!");*/
+
+        // usando do while
+        /*int opcion = 0;
+        
+        do{
+            System.out.println("1 - ingresar");
+            System.out.println("2 - salir");
+            Scanner entrada = new Scanner (System.in);
+            opcion = entrada.nextInt();
+        
+            switch(opcion){
+                case 1 : System.out.println("Inresaste");
+                break;
+                default: System.out.println("Error, numero no valido");
+                break;
+            }
+        } while (opcion != 2);*/
+    }
+}
