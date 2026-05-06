626f802cb7584c57e4e40521069f206ac50c3c6c
Kpi <186628550+DevKpi@users.noreply.github.com>
2026-05-06 20:36:07 -0300

    Implement main functionality in act0605.java

---
+++ act0605.java
@@ -0,0 +1,39 @@
+package kpi;
+
+import java.util.*;
+
+public class act0605 {
+    public static void main (String[] args){
+        MostrarBienvenida();
+        
+        CalcularDescuento(1500, 100);
+        
+        int numTicket = GenerarTicket();
+        System.out.println("Numero de ticket: " + numTicket);
+        
+        int[] numeros = {10, 20, 30, 40, 50};
+        double promedio = CalcularPromedio(numeros);
+        System.out.println();
+        
+    }
+    
+    public static void MostrarBienvenida(){
+        System.out.println("Bienvenido");
+    }
+    
+    public static void CalcularDescuento(int valorTotal, int descuentoAplicado){
+        valorTotal -= descuentoAplicado;
+        System.out.println("El descuento de " + descuentoAplicado + " deja un valor total de " + valorTotal);
+    }
+    
+    public static int GenerarTicket(){
+        Random rand = new Random();
+        int ticket = rand.nextInt(101);
+        return ticket;
+    }
+    
+    public static double CalcularPromedio([]numeros){
+        return ;
+    }
+    
+}
