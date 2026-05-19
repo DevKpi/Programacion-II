b3b81d049e91051084294b2fce39f6fe6f85e45e
Kpi <186628550+DevKpi@users.noreply.github.com>
2026-05-19 20:06:46 -0300

    Create act1906 class with main method for array input
    Implement main method to read array size and input numbers.

---
+++ act1906.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kpi;
+
+import java.util.*;
+
+/**
+ *
+ * @author isft118
+ */
+public class act1906 {
+    public static void main (String[] args){
+        
+        Scanner ent = new Scanner(System.in);
+        int tam = 0;
+        int fin = 0;
+        int i = 0;
+        
+        System.out.println("Ingresa el tamaño del arreglo: ");
+        tam = ent.nextInt();
+        
+        int []arreglo = new int[tam];
+        
+        System.out.println("Ahora para realizar la carga los numeros deben ser positivos o cero.");
+        System.out.println("Si desea cortar la carga ingrese -1");
+        
+        while (fin != -1){
+            System.out.println("Ingrese un numero para la posición " + i);
+            
+            i++;
+        }
+        
+    }
+    
+}
